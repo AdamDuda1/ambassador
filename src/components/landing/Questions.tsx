31176603741256f0ff90b7c6c6fb8781cc57bb51
heygideon <47083178+heygideon@users.noreply.github.com>
2026-04-14 16:36:09 +0100

    fix(landing): update faq copy
    use bullet points for 'what do I get?'

--- src/components/landing/Questions.tsx
+++ src/components/landing/Questions.tsx
@@ -1,73 +1,78 @@
 const questions = [
   {
     question: "What would I do?",
-    answers: [
-      <span key="what">
-        Help spread the word about Hack Club in your country! Put up posters to
-        encourage teens to check out our events, with your unique QR code on
-        them.
-      </span>,
-    ],
-  },
-  {
-    question: "How does this work?",
-    answers: [
-      <span key="how-0">
-        We&rsquo;ve already made some amazing posters for you, so all you have
-        to do is print them out from our platform and put them up for people to
-        see! Please remember to follow any local laws about putting up posters -
-        you could look into places like local shops or community boards.
-      </span>,
-      <span key="how-1">
-        We&rsquo;re planning to add referral links too, but for now it&rsquo;s
-        posters only.
-      </span>,
-    ],
+    answers: (
+      <>
+        <p>
+          Help spread the word about Hack Club in your country! Put up posters
+          to encourage teens to check out our events, with your unique QR code
+          on them.
+        </p>
+      </>
+    ),
   },
   {
     question: "What do I get?",
-    answers: [
-      <span key="what-get-0">
-        We&rsquo;ll give you $1.00 for every poster you put up (with a photo!),
-        and $0.50 for every person who signs up and verifies themselves as under
-        18.
-      </span>,
-      <span
-        key="what-get-1"
-        className="block text-base leading-relaxed text-neutral-600"
-      >
-        example: if you put up 15 posters in a month, and 10 people sign up{" "}
-        <span className="underline">per poster</span>, that&rsquo;s $15 + $75 =
-        $90 for that month!
-      </span>,
-      <span key="what-get-2">
-        You&rsquo;ll get a $20 fund for any supplies like paper, printer ink,
-        pins, and blu-tack that you need to put up your posters - this can be
-        topped up if you need more.
-      </span>,
-      <span key="what-get-3">
-        If you run 2 or more meetups each month in June, July and August,
-        we&rsquo;ll give you a $100 honorarium, paid at the end of August.
-      </span>,
-      <span key="what-get-4">
-        We&rsquo;ll also send you Hack Club branded t-shirts and merch.
-      </span>,
-    ],
+    answers: (
+      <ul className="space-y-2 pl-6 list-disc">
+        <li>
+          <strong>$100 guaranteed honorarium</strong>, as long as you run 2 or
+          more meetups in June, July, and August (paid at the end of August!)
+        </li>
+        <li>
+          $20 fund for any supplies like paper, printer ink, pins, and blu-tack
+          to put up posters - this can be topped up if you need more
+        </li>
+        <li>A Hack Club-branded t-shirt to wear with pride!</li>
+        <li>
+          <p>
+            $1.00 for every poster you put up (with a photo!), and $0.50 for
+            every person who signs up and verifies themselves as under 18
+          </p>
+          <p className="mt-0.5 text-base leading-relaxed text-neutral-600">
+            example: if you put up 15 posters in a month, and 10 people sign up{" "}
+            <span className="underline">per poster</span>, that&rsquo;s $15 +
+            $75 = $90 for that month!
+          </p>
+        </li>
+      </ul>
+    ),
+  },
+  {
+    question: "How does this work?",
+    answers: (
+      <>
+        <p>
+          We&rsquo;ve already made some amazing posters for you, so all you have
+          to do is print them out from our platform and put them up for people
+          to see! Please remember to follow any local laws about putting up
+          posters - you could look into places like local shops or community
+          boards.
+        </p>
+        <p>
+          We&rsquo;re planning to add referral links too, but for now it&rsquo;s
+          posters only.
+        </p>
+      </>
+    ),
   },
   {
     question: "Who can apply?",
-    answers: [
-      <span key="who-0">
-        Teenagers aged 13-18 from the United States, United Kingdom, Canada,
-        Europe and Australia can apply. You&rsquo;ll need to be able to get to
-        places around your city - this may involve walking, biking, driving, or
-        taking public transit, it&rsquo;s up to you.
-      </span>,
-      <span key="who-1">
-        We hope to add more countries in the future, but this is a brand new
-        program, so we&rsquo;re trying it out first in a small number of areas.
-      </span>,
-    ],
+    answers: (
+      <>
+        <p>
+          Teenagers aged 13-18 from the United States, United Kingdom, Canada,
+          Europe and Australia can apply. You&rsquo;ll need to be able to get to
+          places around your city - this may involve walking, biking, driving,
+          or taking public transit, it&rsquo;s up to you.
+        </p>
+        <p>
+          We hope to add more countries in the future, but this is a brand new
+          program, so we&rsquo;re trying it out first in a small number of
+          areas.
+        </p>
+      </>
+    ),
   },
 ] as const;
 
@@ -84,11 +89,7 @@ export default function Questions() {
             className="py-4 border-b border-neutral-300 leading-relaxed text-lg md:text-xl"
           >
             <p className="font-bold">{item.question}</p>
-            {item.answers.map((answer, index) => (
-              <p key={index} className="mt-2">
-                {answer}
-              </p>
-            ))}
+            <div className="mt-2 space-y-2">{item.answers}</div>
           </div>
         ))}
       </div>
